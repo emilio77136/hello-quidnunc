2d14fa50139225d19c3c2d4b6b3e44f7f85d144f
Emilio Lopez <2023158951@ub.edu.bz>
2025-09-11 19:27:30 -0600

    authentication

---
+++ authentication.cpp
@@ -0,0 +1,14 @@
+Authentication
+
+Authentication is basically the way a system checks that you really are who you
+say you are before granting you access to your information. If someone tries to
+impersonate you and fails the authentication process they won't be able to get in. So it's
+a security step designed to keep devices and information away from the wrong people.
+The most common example is the password. While passwords are easy to use they
+also come with risks. They can be weak and easily guessed. Therefore many systems
+now rely on much stronger methods such as Biometric authentication. For example it
+uses fingerprints, face scans or voice recognition. Since these features are unique to a
+specific person they are harder to replicate. Another option are keycards or security
+tokens. These function almost like a digital key and without them access is denied.
+Overall authentication is an essential step in cybersecurity because it helps protect
+accounts data and systems from being compromised by unauthorized people.
